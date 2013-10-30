a4c9e9f86421adafc14feac75d2e22883bb41d6e
Alexander Gigga <alexander.gigga@gmail.com>
2013-10-30 18:18:09 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LengooWebsite::Application.config.secret_token = 'c6ae9339e25d52ba5a5c855dea535ee12c6376a63edb832775ebd64084b207a064b1e2d0d95ab694a962811e0f27a6be823379df34a19b7926e5d98fcd6aacf5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+LengooWebsite::Application.config.session_store :cookie_store, key: '_lengoo_website_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# LengooWebsite::Application.config.session_store :active_record_store
